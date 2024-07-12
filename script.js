012fba61eea323db723ac0f8648641c36244953e
Hanru Vorster <hanru.vorster@gmail.com>
2024-07-12 12:51:00 +0200

    added emojis with unicode to innet.text

--- script.js
+++ script.js
@@ -6,9 +6,9 @@ function validateSyntax() {
     let validation; // here a new variable is defined so we can display the results in the innertext
     // if statement to test if input follows the sequence defined in the regular expression above
     if (sequence.test(input)) {
-        validation = "Valid Syntax"
+        validation = "Valid Syntax \ud83d\udfe2" //using unicode to add emojis
     } else {
-        validation = "Invalid Syntax"
+        validation = "Invalid Syntax \ud83d\udd34"
     }
     document.getElementById('result').innerText = `${validation}`; //here we inject the result into the DOM to display for users
 }
